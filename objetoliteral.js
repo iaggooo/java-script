802053359b4e7183e3346d24cc448520f5562556
iaggooo <iago.matos@escola.pr.gov.br>
2026-06-01 12:08:50 -0300

    objetoliteral.js

--- objetoliteral.js
+++ objetoliteral.js
@@ -1,1 +1,42 @@
 
+function dobra(vetor){
+    let resultados = [];
+    for (let i = 0; i < vetor.length ; i++){
+        resultados.push(vetor[i] * 2);
+    }
+    return resultados;
+}function dobra(vetor){
+    return vetor.map((item) => item * 2);
+
+SELECT * FROM Alunos WHERE Escola=’Alura’;
+function dobra(vetor){
+    let resultados = [];
+    for (let i = 0; i < vetor.length ; i++){
+        resultados.push(vetor[i] * 2);
+    }
+    return resultados;
+}function dobra(vetor){
+    return vetor.map((item) => item * 2);
+}
+SELECT * FROM Alunos WHERE Escola=’Alura’;
+
+let dataEHora = Date() // utilizando a chamada de função diretamente
+console.log(dataEHora)
+ // Mon Jan 31 2022 23:44:05 GMT-0300 (Brasilia Standard Time)
+console.log(typeof dataEHora)
+ // string
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
